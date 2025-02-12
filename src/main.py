5823cbc07ea92ffbe708634874093aafb30472c6
jotica0602 <alejandroeb5271@gmail.com>
2025-02-12 06:28:35 -0500

    modified:   src/main.py

--- src/main.py
+++ src/main.py
@@ -1,11 +1,12 @@
 from controller import ClientController
 # -H <direccion_ip> -p <puerto> -n <nick> -c <comando> -a <argumento>"
 
-test_input = input().replace('"','').split(' ')
+test_input = input()
 print(test_input)
-host,port,nickname,command,arg = test_input[1],int(test_input[3]),test_input[5],test_input[7],test_input[9]
+# print(test_input)
+# host,port,nickname,command,arg = test_input[1],int(test_input[3]),test_input[5],test_input[7],test_input[9]
 
-ircc = ClientController()
-ircc.connect_to_server(host,port,nickname,False)
-ircc.handle_user_input(command + ' ' + arg)
+# ircc = ClientController()
+# ircc.connect_to_server(host,port,nickname,False)
+# ircc.handle_user_input(command + ' ' + arg)
 # ircc.disconnect_from_server()
