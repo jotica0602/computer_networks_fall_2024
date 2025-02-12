102b6798522caed5a108091c0e9f6203d473a332
jotica0602 <alejandroeb5271@gmail.com>
2025-02-12 08:19:56 -0500

    modified:   src/main.py

--- src/main.py
+++ src/main.py
@@ -1,31 +1,123 @@
-from controller import ClientController
-import sys
-import time
-# -H <direccion_ip> -p <puerto> -n <nick> -c <comando> -a <argumento>"
+import socket
+import ssl
+import threading
 
-test_input = sys.argv[1:]
-# print(test_input)
-host,port,nickname,command,arg = test_input[1],int(test_input[3]),test_input[5],test_input[7].split('/'),test_input[9]
-command = '/' + command[len(command) -1 ]
-# print(command)
-# exit()
-ircc = ClientController()
-ircc.connect_to_server(host,port,nickname,False)
-ircc.handle_user_input(command + ' ' + arg)
-time.sleep(10)
-ircc.disconnect_from_server()
+class IRCClient:
+    def __init__(self, server, port, nickname, use_ssl=False):
+        self.server = server
+        self.port = port
+        self.nickname = nickname
+        self.use_ssl = use_ssl
+        self.running = True
+        
+        self.sock = socket.create_connection((server, port))
+        if use_ssl:
+            context = ssl.create_default_context()
+            self.sock = context.wrap_socket(self.sock, server_hostname=host)
+        
+        self.send_raw(f'NICK {self.nickname}')
+        self.send_raw(f'USER {self.nickname} 0 * :Python IRC Client')
+        
+    
+    def send_raw(self, data):
+        self.sock.send((data + '\r\n').encode('utf-8'))
+    
+    def change_nick(self, new_nick):
+        self.send_raw(f'NICK {new_nick}')
+        self.nickname = new_nick
+    
+    def join_channel(self, channel):
+        self.send_raw(f'JOIN {channel}')
+    
+    def part_channel(self, channel):
+        self.send_raw(f'PART {channel}')
+    
+    def send_message(self, target, message):
+        self.send_raw(f'PRIVMSG {target} :{message}')
+    
+    def send_notice(self, target, message):
+        self.send_raw(f'NOTICE {target} :{message}')
+    
+    def list(self):
+        self.send_raw(f'LIST')
+    
+    def quit(self, message="Goodbye!"):
+        self.send_raw(f'QUIT :{message}')
+        self.running = False
+        self.sock.close()
+    
+    def handle_server_response(self):
+        while self.running:
+            try:
+                data = self.sock.recv(4096).decode('utf-8', errors='ignore')
+                if not data:
+                    break
+                for line in data.split('\r\n'):
+                    if line:
+                        print(line)
+                        self.process_message(line)
+            except Exception as e:
+                print(f'Error en la recepción: {e}')
+                break
+    
+    def process_message(self, message):
+        parts = message.split()
+        if len(parts) < 2:
+            return
+        if parts[0] == 'PING':
+            self.send_raw(f'PONG {parts[1]}')
+    
+    def start(self):
+        thread = threading.Thread(target=self.handle_server_response)
+        thread.start()
 
+if __name__ == "__main__":
+    import sys
+    
+    test_input = sys.argv[1:]
+    host,port,nickname,command,arg = test_input[1],int(test_input[3]),test_input[5],'/'+test_input[7].split('/').pop(),test_input[9]
+    
+    client = IRCClient(host, port, nickname)
+    client.start()
+    
+    if command == "/nick":
+        client.change_nick(arg)
+    elif command == "/join":
+        client.join_channel(arg)
+    elif command == "/part":
+        client.part_channel(arg)
+    elif command == "/privmsg":
+        target, message = arg.split(" ", 1)
+        client.send_message(target, message)
+    elif command == "/notice":
+        target, message = arg.split(" ", 1)
+        client.send_notice(target, message)
+    elif command == "/list":
+        client.list()
+    elif command == "/quit":
+        client.quit(arg)
+        
+    client.quit()
 
-
-# Traceback (most recent call last):
-#   File "/home/runner/work/computer_networks_fall_2024/computer_networks_fall_2024/src/main.py", line 10, in <module>
-#     ircc.handle_user_input(command + ' ' + arg)
-#   File "/home/runner/work/computer_networks_fall_2024/computer_networks_fall_2024/src/controller.py", line 42, in handle_user_input
-#     self.handle_user_command(input)
-#   File "/home/runner/work/computer_networks_fall_2024/computer_networks_fall_2024/src/controller.py", line 56, in handle_user_command
-#     self.send_message(command_name + " " + command_args)
-#   File "/home/runner/work/computer_networks_fall_2024/computer_networks_fall_2024/src/controller.py", line 65, in send_message
-#     self.server_interface.send_message(message)
-#   File "/home/runner/work/computer_networks_fall_2024/computer_networks_fall_2024/src/server_interface.py", line 38, in send_message
-#     self.socket.send((message + "\r\n").encode('utf-8'))
-# BrokenPipeError: [Errno 32] Broken pipe
+    # while True:
+    #     cmd = input().strip().split(" ", 1)
+    #     if not cmd:
+    #         continue
+    #     command = cmd[0]
+    #     argument = cmd[1] if len(cmd) > 1 else ""
+        
+    #     if command == "/nick":
+    #         client.change_nick(argument)
+    #     elif command == "/join":
+    #         client.join_channel(argument)
+    #     elif command == "/part":
+    #         client.part_channel(argument)
+    #     elif command == "/privmsg":
+    #         target, message = argument.split(" ", 1)
+    #         client.send_message(target, message)
+    #     elif command == "/notice":
+    #         target, message = argument.split(" ", 1)
+    #         client.send_notice(target, message)
+    #     elif command == "/quit":
+    #         client.quit(argument)
+    #         break
