fa3079f6cc9d18aa2b3825783b8c0b8c5c64b779
jotica0602 <alejandroeb5271@gmail.com>
2025-02-12 10:41:19 -0500

    modified:   src/main.py

--- src/main.py
+++ src/main.py
@@ -419,7 +419,7 @@ def main():
     test_input = sys.argv[1:]
     print(test_input)
     server_ip,port,nickname,command,argument = test_input[1],int(test_input[3]),test_input[5],'/'+test_input[7].split('/').pop(),test_input[9:]
-    print(argument)
+    print(''.join(argument))
     exit()
     # print(server_ip)
     # print(nickname)
